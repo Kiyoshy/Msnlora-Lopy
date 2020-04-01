df40039e912011c780427cd5cadc5a6e718879e2
Pietro Manzoni <manzoni@ieee.org>
2020-04-01 22:41:47 +0200

    Updated

--- testtools/receiver.py
+++ testtools/receiver.py
@@ -1,78 +1,95 @@
-import socket
+import getopt
 import json
 import re
+import socket
+import sys
+import time
 
-import paho.mqtt.client as mqtt
+#
+# When sender is a LoPy
+#
+# import ufun
+# WIFI_SSID = "messenger_e57"
 
-THE_BROKER = "test.mosquitto.org"
+TCP_IP    = '192.168.4.1'
+TCP_PORT  = 80
 
-TCP_IP = '192.168.4.1'
-TCP_PORT = 80
-WIFI_SSID = "messenger_e89"
+USER      = "scarlett"
+REGISTER  = False
 
-BUFFER_SIZE = 1024  # Normally 1024
+def create_POST_msg(type, content):
+    cl = len(content)
+    m  = 'POST ' + type + ' HTTP/1.1\r\n'
+    m += 'Host: '+ TCP_IP + '\r\n'
+    m += 'User-Agent: TestingDevice\r\n'
+    m += 'Content-Length: ' + str(cl) + '\r\n'
+    m += '\r\n'
+    m += content
+
+    return(m)
+
 
 def send_it(m):
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((TCP_IP, TCP_PORT))
-	s.send(m)
-	# simply discarding the response... for the moment
-	dl = []
-	while 1:
-		d = s.recv(BUFFER_SIZE)
-		dl.append(d)
-		if not d: break
-	s.close()
-	dd = b''.join(dl)
-	r = dd.decode()
-	response = r.split('\n')[0]
-	if response == "HTTP/1.1 200 OK":
-	    print("Got: "+response)
-	else:
-	    print("ERROR Got: "+response)
+    BUFFER_SIZE = 1024
 
-	return(r)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    s.send(m.encode())
+
+    dl = []
+    while 1:
+        d = s.recv(BUFFER_SIZE)
+        dl.append(d)
+        if not d: break
+    s.close()
+    dd = b''.join(dl)
+    r = dd.decode()
+    response = r.split('\n')[0]
+    if response == "HTTP/1.1 200 OK":
+        print("Got: "+response)
+    else:
+        print("ERROR Got: "+response)
 
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected to ", client._host, "port: ", client._port)
-    print("Flags: ", flags, "returned code: ", rc)
+def main(argv):
+    global WIFI_SSID, USER, REGISTER
 
-# The callback for when a message is published.
-def on_publish(client, userdata, mid):
-    print("sipub: msg published (mid={})".format(mid))
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'w:r:u:', ['wifissid=', 'register=', 'user='])
+    except getopt.GetoptError:
+        print ('receiver.py -w <WIFI_SSID> [-r|-u] <USER>')
+        sys.exit(2)
 
-client = mqtt.Client(client_id="", 
-                     clean_session=True, 
-                     userdata=None, 
-                     protocol=mqtt.MQTTv311, 
-                     transport="tcp")
+    for opt, arg in opts:
+        if opt in ('-w', '--wifissid'):
+            WIFI_SSID = arg
+        elif opt in ('-r', '--register'):
+            REGISTER = True
+            USER = arg
+        elif opt in ('-u', '--user'):
+            REGISTER = False
+            USER = arg
+        else:
+            print ('receiver.py -w <WIFI_SSID> [-r|-u] <USER>')
+            sys.exit(2)
 
-client.on_connect = on_connect
-client.on_publish = on_publish
 
-#client.username_pw_set(None, password=None)
-client.connect(THE_BROKER, port=1883, keepalive=60)
+if __name__ == "__main__":
 
-rest_msg = 'POST /mqttproxypop HTTP/1.1\r\n'
-rest_msg += 'Host: 192.168.4.1\r\n'
-rest_msg += 'User-Agent: LoPy\r\n'
-rest_msg += 'Content-Length: 13\r\n'
-rest_msg += '\r\n'
-rest_msg += 'Popping data\n'
-pop = send_it(rest_msg)
+    main(sys.argv[1:])
 
-# Extracting json of the sensor data
-b = re.search(".*?\{(.*)\}.*",pop)
-bd = '{' + b.group(1) + '}'
+    if REGISTER:
+        #
+        # registering
+        #
+        print("registering USER: ", USER)
+        rest_msg = create_POST_msg("/registro.html", "sender_name="+USER)
+        send_it(rest_msg)
 
-jinp = json.loads(bd)
+        sys.exit(0)
 
-print(jinp["TOPIC"]+ " with value ", jinp["VALUE"])
+    rest_msg = create_POST_msg("/tabla.html", "dest_name=&user_message=")
+    pop = send_it(rest_msg)
 
-# 	d = {"DEV_ID": DEV_ID, "QOS": QOS, "TOPIC": TOPIC, "VALUE": v}
+    print(pop)
 
-client.loop_start()
-client.publish(jinp["TOPIC"], payload=jinp["VALUE"], qos=jinp["QOS"], retain=False)
-client.loop_stop()
